688959750eb1e64af121a9b77a7465f8560a12e0
leegyeongho <dhkscjsrudgh@gmail.com>
2024-07-10 21:55:08 +0900

    stack_complete

---
+++ stack.py
@@ -0,0 +1,37 @@
+#%%
+class Node:
+    def __init__(self,value):
+        self.value = value 
+        self.next = None 
+
+class Stack:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.top = new_node 
+        self.height = 1 
+
+    def push(self,value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node 
+        else:
+            new_node.next = self.top 
+            self.top = new_node 
+        self.height += 1 
+
+    def pop(self):
+        if self.height == 0:
+            return None  
+        temp = self.top 
+        self.top = self.top.next 
+        temp.next = None
+        self.height -= 1
+        return temp   
+
+    def print_stack(self):
+        temp = self.top 
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next 
+
+        
