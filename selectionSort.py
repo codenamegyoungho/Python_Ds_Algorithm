ab04b3c92faaf81bd54332e74be1da3151a2a443
leegyeongho <dhkscjsrudgh@gmail.com>
2024-07-18 09:07:20 +0900

    selectionSort_complete

---
+++ selectionSort.py
@@ -0,0 +1,15 @@
+#%%
+def selection_sort(my_list):
+    for i in range(len(my_list) - 1):
+        min_index = i 
+        for j in range(i+1,len(my_list)): 
+            if my_list[min_index] > my_list[j]:
+                min_index = j 
+        if i != min_index:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index] 
+            my_list[min_index] = temp 
+    return my_list 
+
+my_list = [5,4,3,2,1]
+selection_sort(my_list)
